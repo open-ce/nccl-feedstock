80996a1c9c3a0008972a50dc5d3f304f989f09d0
Bryant Nelson <bryant.nelson@ibm.com>
2020-09-04 14:13:22 -0500

    Initial code copy. (#1)

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # nccl-feedstock
+Feedstock for NCCL package
---
+++ recipe/build.sh
@@ -0,0 +1,15 @@
+# *****************************************************************
+#
+# Licensed Materials - Property of IBM
+#
+# (C) Copyright IBM Corp. 2018, 2019. All Rights Reserved.
+#
+# US Government Users Restricted Rights - Use, duplication or
+# disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+#
+# *****************************************************************
+export CUDA_HOME=$PREFIX
+
+make -j src.build
+rm -rf build/obj/
+cp -R build/* $PREFIX/
