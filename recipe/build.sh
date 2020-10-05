4a616f2db1e0b6d0c6600746a904e3192dc3b68d
Jason Furmanek <furmanek@us.ibm.com>
2020-10-05 17:17:09 +0000

    Fix recipe so it uses the nvcc package properly

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,15 @@
 #
 # *****************************************************************
 
+## The make environment in NCCL is fairly configurable which is good
+## because we want to use the `nvcc` wrapper package in Open-CE.
+## Setting CUDA_HOME here to the conda environment will work for that. 
+## NCCL, however, also uses some static libraries from the CUDA Toolkit
+## and so we'll use it's CUDA_LIB env variable to tell it about the local
+## installation as well.
+
 export CUDA_HOME=$CONDA_PREFIX
+export CUDA_LIB=${OPEN_CE_CUDA_HOME}/lib64
  
 make -j src.build
 rm -rf build/obj/
